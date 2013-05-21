f55443566c747fb92d6a6b5a151c036ba5c87f07
Augustina Ragwitz <auggy@cpan.org>
2013-05-21 01:48:11 +0000

    Import of AUGGY/Acme-Auggy-0.001 from CPAN.
    gitpan-cpan-distribution: Acme-Auggy
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         AUGGY/Acme-Auggy-0.001.tar.gz
    gitpan-cpan-author:       AUGGY
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/Auggy.pm
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+package Acme::Auggy;
+
+sub say_auggy {
+    return "Auggy!";
+}
+
+1;
+
+# ABSTRACT: Just a module for my talk
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Acme::Auggy - Just a module for my talk
+
+=head1 VERSION
+
+version 0.001
+
+=head1 METHODS
+
+=head2 print_auggy
+
+Just prints my name ;)
+
+=head SYNOPSIS
+
+This module is for demonstration purposes only.
+
+=head1 AUTHOR
+
+Augustina Ragwitz <auggy@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Augustina Ragwitz.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
